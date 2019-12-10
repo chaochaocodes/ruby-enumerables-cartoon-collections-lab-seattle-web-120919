5479e707583c417e196974004bcef106777a8d36
Alicia Chao <aliciachao@gmail.com>
2019-12-10 06:13:51 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,10 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(dwarf_array)# code an argument here
   # Your code here
+  dwarf_array.each_with_index
+  do |dwarf, i|
+      puts dwarf
+      i += 1
+  end 
 end
 
 def summon_captain_planet# code an argument here
