4b9fbe430f89aeb3c8fcaf94df5eb406e5276280
Alicia Chao <aliciachao@gmail.com>
2019-12-10 06:30:20 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,17 +1,22 @@
 def roll_call_dwarves(dwarf_array)# code an argument here
-  # Your code here
+  # iterates and prints numbered list of dwarves, starting at 1. and not 0!
     (dwarf_array).each_with_index {|dwarf, i| puts "#{i+1}. #{dwarf}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(team)# code an argument here
+  # returns an array with the same number of elements that it was given
+  #   capitalizes each element and adds an exclamation mark
+  (team).map {|team| team.capitalize + "!"}  
 end
 
 def long_planeteer_calls# code an argument here
-  # Your code here
+  # returns true if any calls are longer than 4 characters (FAILED - 4)
+  #     returns false if all calls are 4 characters or less (FAILED - 5)
 end
 
 def find_the_cheese# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  # returns the first element of the array that is cheese (FAILED - 6)
+  #   returns nil if the array does not contain a type of cheese (FAILED - 7)
 end
